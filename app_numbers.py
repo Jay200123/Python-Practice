f3514dab61c9b1025e3254fb527147d277a61449
Jay200123 <kenken.ayonon@gmail.com>
2023-05-07 15:36:36 +0800

    commit

---
+++ app_numbers.py
@@ -0,0 +1,22 @@
+from math import *
+
+my_Num = 7
+
+print(my_Num)
+
+print(abs(my_Num)) #returns the absolute number
+
+num1 = 3
+num2 = 2
+num = print("The square root of 3 and 2 is:",pow(num1, num2)) #performs a √ or square root function
+
+print(sqrt(36)) #gives the square root of a number
+
+value1=23
+value2=7
+print("The Big Number is",max(value1, value2)) #returns the bigger number
+print("The Small Number is",min(value1, value2)) #returns the smaller number
+
+
+print(round(21.8888)) #performs a roundoff function
+print(round(36.7778))
---
+++ index.py
@@ -0,0 +1,33 @@
+from math import *
+
+message="Hello!"
+name="Renyel Jay"
+print(message, name)
+
+lname = "SIOC"
+print(lname.lower())
+print(lname.lower().islower())
+
+fname='renyeljay'
+print(fname.upper())
+print(fname.upper().isupper())
+
+full_Name=["Renyel Jay","Jimunsala","Sioc"]
+
+print("First Name:", full_Name[0])
+print("Middle Name:", full_Name[1])
+print("Last Name:", full_Name[2])
+
+val1=7
+print("The Absolute Value for:",val1, "is:", abs(val1))
+
+num1=23
+num2=21
+
+print("The Big Number is:", max(num1, num2))
+print("The Smaller Number is:", min(num1, num2))
+
+print(pow(3,2)) #performs a 3 ^ 2 function
+print(sqrt(9)) #gives the square root of a number
+
+print(round(36.7))
