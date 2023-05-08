987fc7a8cc6c336cf6e4fde64f356a164c892155
Jay200123 <kenken.ayonon@gmail.com>
2023-05-08 15:23:23 +0800

    list

---
+++ list.py
@@ -0,0 +1,20 @@
+names = ['Renyel','Ian','Neri','Edmon']
+1
+print(names)
+
+print("1st Name:", names[0])
+print("2nd Name:", names[1])
+print("3rd Name:", names[2])
+print("4th Name:", names[3])
+
+# counts the list backwards1
+print("Last Name:", names[-1])
+print("Second Last Name:", names[-2])
+
+# specify the list range 
+print(names[1:])
+print(names[1:3])
+
+#update the list content
+names[0] = "Renyel Jay"
+print(names[0])
