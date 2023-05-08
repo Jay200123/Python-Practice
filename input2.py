4649d2315bd2748ead39680b1a2e47d10ab57778
Jay200123 <kenken.ayonon@gmail.com>
2023-05-08 21:47:06 +0800

    add months

--- input2.py
+++ input2.py
@@ -2,18 +2,38 @@ print("Calculating Monthly Wage")
 rate_Hrs = float(input("Enter Rate Per Hours: "))
 work_Hrs = int(input("Enter Work Hours: "))
 dayOffs = int(input("Enter Numbers of DayOff: "))
+month = input("Enter A Month: ")
 
-month = 31
+if month=='January':
+    monthDays = 31
+    
+elif month == 'February':
+    monthDays = 28
+    
+elif month == 'March':
+    monthDays = 31
 
+elif month=='April':
+    monthDays = 30
+    
+elif month=='May':
+    monthDays = 31  
+      
+else:
+    print("Enter a Valid Month")    
+    
+    
 daily_Wage = rate_Hrs * work_Hrs
 deductions = dayOffs * daily_Wage
-monthly_Wage = daily_Wage * month
+monthly_Wage = daily_Wage * monthDays
 salary = monthly_Wage - deductions
 
-print("Salary Report")
-print("Number of Days For Month of May:", month, "Days")
+
+print("Salary Report for the Month of ", month)
+print("Number of Days For Month of", month,":", monthDays,"Days")
 print("Number of DayOffs:", dayOffs)
 print("Daily Wage:", daily_Wage)
 print("Monthly Wage without Deductions:", round(monthly_Wage))
 print("Salary Deductions:",deductions)
 print("Overall Total Salary:", round(salary))
+
